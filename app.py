c231f65952cea16e03880b4d9b305f4b2f00469f
doodle-aditya <aditya0123456@gmail.com>
2025-05-09 10:45:15 +0530

    Minor changes

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 
-
+# Fucntion to add new category
+def add_category():
+    pass
+# Function to add expense
+def add_expense():
+    pass
 # Fucntion to check categories wise expenses
 def cat_expense():
     pass
@@ -18,11 +23,22 @@ def main():
         4. Add new category
         5. Exit the Application
 ''')# Welcome Notice 
+    
     command = int(input('Enter Your Transaction number')) # Input of the user
+    # Task verification
     if command==1:
         total_expense()
     elif command==2:
         cat_expense()
+    elif command==3:
+        add_expense()
+    elif command==4:
+        add_category()
+    elif command==5:
+        print('Thanks For using us....')
+    else:
+        print('Invalid Input Please check the input and proceed')
+        main()
 
 if __name__ == '__main__':
     main()
