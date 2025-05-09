392f1d51bf1c61931825012f3407fba345d4794f
doodle-aditya <aditya0123456@gmail.com>
2025-05-09 11:08:40 +0530

    Added Database and Add expenses functionality

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+import sqlite3 as sql # Importing the library to use database
 
+# Creating database and table for storage.
+conn = sql.connect('Database.db')
+curr = conn.cursor()
+curr.execute('''
+             CREATE TABLE IF NOT EXISTS
+             expense(
+             categories varchar(50),
+             amount int(20),
+             description varchar(50))
+             ''')
+conn.commit()
 # Fucntion to add new category
 def add_category():
     pass
 # Function to add expense
-def add_expense():
-    pass
+def add_expense(category,amount,description):
+    # Adding expense in Database
+    curr.execute('''INSERT INTO expense(categories,amount,decription) VALUES (?,?,?)'''(category,amount,description))
+    conn.commit()
+    print('Expense Added Successfully')# Notice for successful transaction
+
 # Fucntion to check categories wise expenses
 def cat_expense():
     pass
@@ -25,17 +41,26 @@ def main():
 ''')# Welcome Notice 
     
     command = int(input('Enter Your Transaction number')) # Input of the user
+
     # Task verification
     if command==1:
         total_expense()
+
     elif command==2:
         cat_expense()
+
     elif command==3:
-        add_expense()
+        category = input('Enter in which category you want to enter')
+        amount = int(input('Enter the amout you want to enter'))
+        description = input('Give the description of the expense')
+        add_expense(category,amount,description)
+
     elif command==4:
         add_category()
+
     elif command==5:
         print('Thanks For using us....')
+
     else:
         print('Invalid Input Please check the input and proceed')
         main()
