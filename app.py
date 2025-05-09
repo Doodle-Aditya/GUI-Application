42faea820de5ff573b26bd75f89879c1009ae4c8
doodle-aditya <aditya0123456@gmail.com>
2025-05-09 11:19:05 +0530

    Added all the function

--- app.py
+++ app.py
@@ -11,9 +11,14 @@ curr.execute('''
              description varchar(50))
              ''')
 conn.commit()
-# Fucntion to add new category
-def add_category():
-    pass
+# Fucntion to list all category
+def list_category():
+    curr.execute("SELECT DISTINCT categories FROM expense")
+    categories = curr.fetchall()
+    print("Categories:")
+    for cat in categories:
+        print(f"- {cat[0]}")
+
 # Function to add expense
 def add_expense(category,amount,description):
     # Adding expense in Database
@@ -23,10 +28,17 @@ def add_expense(category,amount,description):
 
 # Fucntion to check categories wise expenses
 def cat_expense():
-    pass
+    curr.execute('''SELECT sum(amount) FROM expense GROUP BY categories''')
+    expense = curr.fetchall()
+    print(expense)
+
+
 # Function to check total expense
 def total_expense():
-    return
+    curr.execute('''SELCET sum(amount) FROM expense''')
+    amount = curr.fetchall()
+    print(f'Total expense: {amount}')
+
 
 # Main function which will the start point of the app
 def main():
@@ -36,34 +48,37 @@ def main():
         1. Check Total Expense
         2. Categories Wise Expense
         3. Add new Expense 
-        4. Add new category
+        4. List all categories
         5. Exit the Application
 ''')# Welcome Notice 
     
     command = int(input('Enter Your Transaction number')) # Input of the user
+    try:
+        # Task verification
+        if command==1:
+            total_expense()
 
-    # Task verification
-    if command==1:
-        total_expense()
+        elif command==2:
+            cat_expense()
 
-    elif command==2:
-        cat_expense()
+        elif command==3:
+            category = input('Enter in which category you want to enter')
+            amount = int(input('Enter the amout you want to enter'))
+            description = input('Give the description of the expense')
+            add_expense(category,amount,description)
 
-    elif command==3:
-        category = input('Enter in which category you want to enter')
-        amount = int(input('Enter the amout you want to enter'))
-        description = input('Give the description of the expense')
-        add_expense(category,amount,description)
+        elif command==4:
+            list_category()
 
-    elif command==4:
-        add_category()
+        elif command==5:
+            print('Thanks For using us....')
 
-    elif command==5:
-        print('Thanks For using us....')
+        else:
+            print('Invalid Input Please check the input and proceed')
+            main()
+    except ValueError:
+        print('Value error enter a valid value')
 
-    else:
-        print('Invalid Input Please check the input and proceed')
-        main()
 
 if __name__ == '__main__':
     main()
